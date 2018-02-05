3f6d49a171482d059b33160178a5a29a8848d406
yuxianda <yxd9721@qq.com>
2018-02-06 00:10:03 +0800

    2018-2-6 00:10:02

--- application/zx_apply/controller/Index.php
+++ application/zx_apply/controller/Index.php
@@ -21,12 +21,12 @@ class Index extends Common {
 	 * @return void|string
 	 */
 	public function tt() {
-		$t =array_search("", [
+		$t = array_search ( "", [ 
 				"华为",
 				"中兴",
-				"OLT"
-		]);
-		return dump($t?$t:null);
+				"OLT" 
+		] );
+		return dump ( $t ? $t : null );
 	}
 	
 	/**
@@ -75,71 +75,16 @@ class Index extends Common {
 				"msg" => $msg 
 		] );
 	}
-	/**
-	 * get:加载数据到handsontable并验证,
-	 * post:上传,后台处理入库
-	 * 1.默认post为新申请,移除ip、vlan信息再保存,
-	 * 严格验证，不合规不许提交，标记status：0
-	 * 2.带post参数type=import,视为旧信息导入,
-	 * 生成ip表（全）和vlan表信息（不全）。直接入库，并标记tags:导入
-	 * 3.为了新增字段不修改数据库，将新增字段用json保存到一列。
-	 * 在csv转数组时，需要获取额外的字段
-	 *
-	 * @return void|string|mixed|string
-	 */
-	public function _ht_apply() {
-		if (request ()->isPost ()) {
-			$postData = input ( "post.data" );
-			// return dump($postData);
-			$type = input ( "post.type" );
-			$zxInfoTitle = input ( "post.zxInfoTitle", null, null );
-			$zxInfoTitle = json_decode ( $zxInfoTitle, JSON_UNESCAPED_UNICODE );
-			$dataHeader = $this->getHeader ( $zxInfoTitle ["label"], $zxInfoTitle ["order"], true );
-			// 获取数据库的列名
-			$dataHeader = explode ( ",", $dataHeader );
-			// 根据列名和数据转成php数组
-			// $postData = substr ( $postData, 3 ); // 莫名奇妙的前三个字节是垃圾数据。3天才研究出来，只能这样解决！！！
-			$data = $this->csv_to_array ( $dataHeader, $postData );
-			// return dump($data);
-			// 获取额外的字段
-			$extraHeader = config ( "extraInfo" );
-			foreach ( $data as $k => $v ) {
-				$temp = [ ];
-				foreach ( $extraHeader as $kk => $vv ) {
-					$temp [$kk] = $v [$vv];
-				}
-				$data [$k] ["extra"] = $temp;
-				if ($v ["aStation"] == "柴河局") {
-					$data [$k] ["aStation"] .= "-" . $data [$k] ["neFactory"];
-				}
-			}
-			// 若导入，ip/vlan信息要单独存储。
-			$result = Infotables::createInfo ( $data, $type );
-			// $result = $info->save($data[0]);
-			return dump ( $result );
-		}
-		if (request ()->isGet ()) {
-			if (input ( '?get.zxInfoTitle' ) && input ( '?get.t' )) {
-				return $this->fetch ();
-			}
-		}
-	}
 	
 	/**
-	 * ip、vlan申请
-	 *
-	 * @return mixed|string
+	 * 数据专线申请开通
 	 */
 	public function apply() {
-		// $zxInfoTitle = Db::table("phpweb_sysinfo")->field("value,option")->where("label","zxInfoTitle")->select();
 		$zxInfoTitle = [ 
-				"label" => "zx_apply-223-rb",
-				"order" => "0,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,30,31,32,33,34,35,36,37,38,39" 
+				"label" => "zx_apply-new-rb",
+				"order" => "1,2,3,4,5,6,7,8,12,13,14,15,16,17,18,20,21,30,31,32,33,34,35,36,37,38,39" 
 		];
-		$this->assign ( 'zxInfoTitle', json_encode ( $zxInfoTitle, 256 ) );
-		return $this->fetch ();
 	}
-	
 	/**
 	 * 根据label、order 获取表格的 header
 	 * $v为false，获取option(default)；为ture，获取value
